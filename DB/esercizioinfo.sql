68a5f79f084d77a18a6fe99e21ecd82734c56b79
marcy0157 <66092947+marcy0157@users.noreply.github.com>
2022-10-21 20:52:36 +0200

    FIRST

---
+++ DB/esercizioinfo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `articolo` (
+  `codiceArticolo` int(11) NOT NULL,
+  `titolo` varchar(100) NOT NULL,
+  `sottoTitolo` varchar(500) NOT NULL,
+  `testo` text NOT NULL,
+  `dataStesura` date NOT NULL,
+  `pubblicato` tinyint(1) DEFAULT 1,
+  `genere` enum('natura','sport','tecnologia') DEFAULT NULL,
+  `autore` int(11) NOT NULL
+)
