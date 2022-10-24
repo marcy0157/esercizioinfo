8d656f4eec8f6b4d48f9b49668f70243561a6826
marcy0157 <66092947+marcy0157@users.noreply.github.com>
2022-10-24 19:15:08 +0200

    FIRST

--- DB/esercizioinfo.sql
+++ DB/esercizioinfo.sql
@@ -1,11 +1,58 @@
-CREATE TABLE `articolo` (
-  `codiceArticolo` int(11) NOT NULL,
-  `titolo` varchar(100) NOT NULL,
-  `sottoTitolo` varchar(500) NOT NULL,
-  `testo` text NOT NULL,
-  `dataStesura` date NOT NULL,
-  `pubblicato` tinyint(1) DEFAULT 1,
-  `genere` enum('natura','sport','tecnologia') DEFAULT NULL,
-  `autore` int(11) NOT NULL
-  Non sono stato io;
-)
+CREATE TABLE articolo
+(
+    IdArticolo        int auto_increment primary key,
+    Titolo            varchar(10) not null ,
+    Testo             varchar(1000) not null ,
+    DataPubblicazione datetime not null ,
+    Genere enum('Sport', 'Scienza', 'Politica','Informatica','Attualità','Altro') not null ,
+    immagine1 varchar(50) null ,
+    immagine2 varchar(50) null
+
+);
+
+
+CREATE TABLE pagina
+(
+    IdArticolo int auto_increment primary key,
+    IdLink int,
+
+    FOREIGN KEY (IdLink) REFERENCES link(IdLink)
+
+
+
+);
+
+
+CREATE TABLE link (
+    IdLink int auto_increment primary key,
+    link varchar(50) unique not null
+
+
+);
+
+
+
+
+
+CREATE TABLE ArticoloAutore
+(
+    IdArticolo int,
+    IdAutore   int,
+    primary key (IdArticolo, IdAutore),
+
+    FOREIGN KEY (IdArticolo) REFERENCES articolo (IdArticolo),
+    FOREIGN KEY (IdAutore) REFERENCES autore (IdAutore)
+
+
+);
+
+
+CREATE TABLE autore
+(
+    IdAutore     int auto_increment primary key,
+    Nome         varchar(10) not null ,
+    Cognome      varchar(10) not null ,
+    LuogoNascita varchar(20) not null
+
+
+);
